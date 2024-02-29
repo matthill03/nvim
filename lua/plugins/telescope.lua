c5816095d896884289be36a6253bc3d569a73bd4
Matthew Hill <contact@matthew-hill.net>
2024-02-29 19:54:18 +0000

    Adding tmux navigation and keymaps for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,8 +4,13 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+
+      vim.keymap.set('n', '<leader>ff', function() builtin.find_files({no_ignore=true}) end, {})
+      vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fgc', function() builtin.live_grep({search_dirs={vim.fn.expand("%:p")}}) end, {})
+
     end
   },
   {
